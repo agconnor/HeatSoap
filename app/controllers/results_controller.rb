98189ce0cace6eb49573e694ba9eb5da73f3949b
Alex Connor <agconnor@gmail.com>
2010-11-14 23:10:11 -0500

    added forms for selecting parameters

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,9 +1,17 @@
 class ResultsController < ApplicationController
 	def index
-		@table = Result.find_by_sql(["select r.*, h1.mean_los_pos los1, h2.mean_los_pos los2 from (SELECT source_id, target_id, sum(population) as mrsa FROM `results` WHERE (timestep >= ? and timestep <= ?) GROUP BY source_id, target_id) as r "+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def vis
+    @instance_var = "this is a class param";
+    @@static_var = "this is a static param";
+    #@days = Result.find_by_sql(["select distinct timestep from results where experiment_id=1"]);
+		@table = Result.find_by_sql(["select r.*, h1." + params[:plot] +" los1, h2." + params[:plot] + " los2 from (SELECT source_id, target_id, " + params[:agg] + "(population) as mrsa FROM `results` WHERE (timestep >= ? and timestep <= ?) GROUP BY source_id, target_id) as r "+
       " inner join hospitals h1 on r.source_id=h1.id inner join hospitals h2 on r.target_id=h2.id", params[:start], params[:end]])
     respond_to do |format|
-			format.html
       format.json
 		end
 	end
